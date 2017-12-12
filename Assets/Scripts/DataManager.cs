598320b230260efdf2c15cee20ba817a364bcc57
colinstuartsmith <33232629+colinstuartsmith@users.noreply.github.com>
2017-12-12 14:17:21 -0500

    Hyperball beginning

---
+++ Assets/Scripts/DataManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+	public int p1Score = 0;
+	public int p2Score = 0;
+	public int winningScore = 10;
+	public string winningmessage = "";
+}
