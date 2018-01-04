04ba31fb76b7b51166f9d2b2054fca80ba9ce032
colinstuartsmith <33232629+colinstuartsmith@users.noreply.github.com>
2018-01-04 16:07:49 -0500

    Created Fade effect on Studio Logo

--- Assets/Scripts/Launcher.cs
+++ Assets/Scripts/Launcher.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class Launcher : MonoBehaviour 
+public class LaodScene : MonoBehaviour 
 {
 
 	public string StartingScene = "";
