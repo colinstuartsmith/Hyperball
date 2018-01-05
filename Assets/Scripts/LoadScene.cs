a93fb08ff88795648222ff2aec4ca1c28cce58d7
colinstuartsmith <33232629+colinstuartsmith@users.noreply.github.com>
2018-01-05 17:23:39 -0500

    Finished Studio Logo, Fixed load scene and started adding animated title screen

--- Assets/Scripts/LoadScene.cs
+++ Assets/Scripts/LoadScene.cs
@@ -3,14 +3,28 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class LoadScene : MonoBehaviour 
+public class LoadScene : MonoBehaviour
 {
+    // Allows us to specify which scene to load.
+    public string SceneToLoad = "";
 
-	public string StartingScene = "";
+    // Allows us to specify a delay before we load the scene.
+    public float Delay = 0;
 
-	void Start()
-	{
-		SceneManager.LoadScene (StartingScene);
-	}	
+    // This Unity method is called once after the Awake method and before the game loop starts.
+    void Start()
+    {
+        // Start the coroutine which is used to implement the delay and then load the scene.
+        StartCoroutine(delayThenLoadScene());
+    }
 
+    // This coroutine implements a delay and then loads the scene.
+    IEnumerator delayThenLoadScene()
+    {
+        // Implement the specified delay.
+        yield return new WaitForSeconds(Delay);
+
+        // Load the scene.
+        SceneManager.LoadScene(SceneToLoad);
+    }
 }
